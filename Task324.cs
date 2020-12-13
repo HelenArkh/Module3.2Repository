9dd7e85cbfd602e516ec8886533687bdb9a3fff1
HelenArkh <alena.arkh@gmail.com>
2020-12-13 19:44:11 +0300

    Ура, я инициализировала, а потом изменила переменную

---
+++ Task324.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class MainClass
+{
+	public static void Main(string[] args)
+	{
+		string myName;
+		myName = "Helen";
+
+		Console.WriteLine(myName);
+
+		myName = "Jane";
+
+		Console.WriteLine(myName);
+
+		Console.ReadKey();
+	}
+}
