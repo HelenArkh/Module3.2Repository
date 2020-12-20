55b237d9a7140ebf361166364d987aea833834ec
HelenArkh <alena.arkh@gmail.com>
2020-12-20 13:24:54 +0300

    Создание проекта и выполнение задания 4.1.4

---
+++ Module4Practice/Module4Practice/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Module4Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string A;
+            string B;
+
+            bool C = A != B;
+
+            //Console.WriteLine(C);
+            //Console.ReadKey();
+        }
+    }
+}
